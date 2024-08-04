c4ed577bae0c022a8983bc6c188388f4bb0b225c
sel-jett <sel-jett@e3r11p10.1337.ma>
2024-08-04 22:40:27 +0100

    donne ex04

--- C01/ex04/Sed.cpp
+++ C01/ex04/Sed.cpp
@@ -6,50 +6,50 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:05:53 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/07/05 13:19:15 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/08/04 22:29:34 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
 
-#include <fstream>
-
 Sed::Sed(const std::string &filename, const std::string &s1, const std::string &s2): filename(filename), s1(s1), s2(s2)
 {
-    std::cout << "Sed's Contructed is called !!" << std::endl;
+	std::cout << "Sed's Contructed is called !!" << std::endl;
 }
 
 Sed::~Sed()
 {
-    std::cout << "Sed being destructed !!" << std::endl;
+	std::cout << "Sed being destructed !!" << std::endl;
 }
 
 void Sed::search_and_replace()
 {
-    std::string     text;
-    std::string     fullText;
-
-    std::ifstream fs(this->filename);
-    if (fs.fail())
-    {
-        std::cout << "\033[31m" << this->filename << " failed to open !!!" << std::endl;
-        return ;
-    }
-    while (getline(fs, text))
-    {
-        if (!fullText.empty() && !text.empty())
-            fullText = fullText + '\n' + text;
-        else if (!text.empty())
-            fullText = fullText + text;
-    }
-    if (fullText.empty())
-    {
-        std::cout << "Error: file is empty" << std::endl;
-        return ;
-    }
-    std::ofstream ostrm(this->filename + ".replace");
-    if (ostrm.good())
-        std::cout << "\033[31m" << "goooooood !!!" << std::endl;
-    // std::cout << "\033[32m" << fullText << std::endl;
-    
+	if (!std::freopen((this->filename).c_str(), "r", stdin) || \
+			!std::freopen(((this->filename + ".replace")).c_str(), "w+", stdout))
+		std::cout << "failed to load the new file :(" << std::endl;
+	else
+	{
+		std::string text;
+		std::getline(std::cin, text, '\0');
+		if (text.empty())
+		{
+			std::cout << "empty file" << std::endl;
+			return ;
+		}
+		while (!text.empty())
+		{
+			int s1 = text.find(this->s1);
+			if (s1 == -1)
+			{
+				std::cout << text;
+				text = text.substr(text.length());
+			}
+			else
+			{
+				std::cout << text.substr(0, s1) << this->s2;
+				text = text.substr(s1 + (this->s1).length());
+			}
+		}
+		(1) && (fclose(stdout), fclose(stdin));
+	}
 }
--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,38 +6,20 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 10:33:52 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/07/05 11:15:02 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/08/04 22:39:36 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <iostream>
-
 #include "Sed.hpp"
 
-int main (void)
+int main (int ac, char **av)
 {
-    std::string     filename;
-    std::string     s1;
-    std::string     s2;
-    
-    while (filename.empty())
-    {
-        std::cout << "enter a file name : ";
-        getline(std::cin, filename);
-    }
-    while (s1.empty())
-    {
-        std::cout << "enter a string to be replaced : ";
-        getline(std::cin, s1);
-    }
-    while (s2.empty())
-    {
-        std::cout << "enter a string to replace with : ";
-        getline(std::cin, s2);
-    }
+	std::string filename;
+	std::string s1;
+	std::string s2;
 
-    Sed sed(filename, s1, s2);
-    sed.search_and_replace();
-    // fs.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
-    std::cout << "filename : " << filename << " s1 : " << s1 << " s2 : " << s2 << std::endl;
+	if (ac != 4)
+		std::cerr << "invalid number of arguments " << std::endl;
+	Sed sed(av[1], av[2], av[3]);
+	sed.search_and_replace();
 }
