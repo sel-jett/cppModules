829f80f46636de27cec5b4d544474c8c1c7b514e
Salah Eddine El Jettioui <sel-jett@e3r11p4.1337.ma>
2024-08-07 02:33:58 +0100

    init C02

---
+++ C02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 02:23:01 by sel-jett          #+#    #+#             */
+/*   Updated: 2024/08/07 02:27:54 by sel-jett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+    private:
+        int int_value;
+        static const int fractional_value;
+    public:
+        Fixed();
+         
+};
+
+#endif
