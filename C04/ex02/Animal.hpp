48e1954bfc504955dcddf82da5bc8f47540c1048
Salah Eddine El Jettioui <sel-jett@e2r13p4.1337.ma>
2024-08-21 09:30:35 +0100

    fixed pure virtual function in make sound ex02

--- C04/ex02/Animal.hpp
+++ C04/ex02/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 15:52:19 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/08/20 09:51:00 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/08/21 09:29:33 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class Animal {
         Animal();
         Animal(const Animal& obj);
         Animal& operator=(const Animal& obj);
-        virtual ~Animal() = 0;
+        virtual ~Animal();
         Animal(const std::string& type);
         std::string getType( void ) const;
-        virtual void makeSound() const;
+        virtual void makeSound() const = 0;
     
 };
