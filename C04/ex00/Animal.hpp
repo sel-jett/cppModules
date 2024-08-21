9e4b4bdbe31ac5ba1cfe07d94641d9f58b5513b8
Salah Eddine El Jettioui <sel-jett@e2r13p4.1337.ma>
2024-08-21 09:23:20 +0100

    Fixed e00

--- C04/ex00/Animal.hpp
+++ C04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-jett <sel-jett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 15:52:19 by sel-jett          #+#    #+#             */
-/*   Updated: 2024/08/19 20:39:25 by sel-jett         ###   ########.fr       */
+/*   Updated: 2024/08/21 09:22:07 by sel-jett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ class Animal {
         virtual ~Animal();
         Animal(const std::string& type);
         std::string getType( void ) const;
-        void makeSound() const;
+        virtual void makeSound() const;
     
 };
