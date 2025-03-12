d46a9b87b2fe56527a9fbec3e35aaf4d7206ba34
sel-jett <eljettiouisalaheddine@gmail.com>
2025-03-12 01:56:33 +0000

    everything good

--- C06/ex02/main.cpp
+++ C06/ex02/main.cpp
@@ -1,11 +1,12 @@
 #include "Base.hpp"
 
+
 int main ( void ) {
     Base *first = generate();
     identify(first);
-    delete(first);
     // Base *second = generate();
     identify(*first);
+    delete(first);
     // identify(*second);
     // delete(second);
     return (0);
